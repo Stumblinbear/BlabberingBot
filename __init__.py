a0ecc30954afe599396ae2a2daa0975d64a59b81
Trevin Miller <stumblinbear@gmail.com>
2018-01-21 15:55:21 +0000

    Major rework

--- __init__.py
+++ __init__.py
@@ -1,151 +1,105 @@
 import time
-import threading
-from random import random
-import re
-import sys
+import asyncio
 
-import nltk
-from nltk.tag import pos_tag
 import telepot
-from telepot.delegate import per_chat_id, create_open, pave_event_space
+from telepot.aio.loop import MessageLoop
+from telepot.aio.delegate import per_chat_id, create_open, pave_event_space
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
 
 
-nltk.download('averaged_perceptron_tagger')
+import botan
 
-reply_request = []
-last_msg_id = {}
-last_user_id = {}
 
-def get_query(the_input):
-	query = the_input
+botan.set_key('06de9876-3ab0-49a7-8772-bd9ca817bc82')
 
-	tags = pos_tag(query.split())
-	def get_search_pos(tags):
-		tag_priority = ['NN', 'NNP', 'PRP', 'WP']
+bot = ChatBot(
+    'Birden',
+    storage_adapter='chatterbot.storage.MongoDatabaseAdapter', 
+    database='chatterbot-database',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.BestMatch',
+#        {
+#            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+#        'threshold': 0.65,
+#            'default_response': 'I-I\'m sorry I... I don\'t know how you want me to respond...'
+#        }
+    ]
+)
 
-		i = 0
-		while i < len(tag_priority):
-			test = [word for word, pos in tags if pos == tag_priority[i]]
-			if len(test) != 0:
-				return test[len(test) - 1]
-			i += 1
+bot.set_trainer(ListTrainer)
+#with open('log.txt', 'r') as f:
+#	bot.train(f.readlines())
 
-		return None
+#bot.set_trainer(ChatterBotCorpusTrainer)
+#bot.train("chatterbot.corpus.english")
 
-	search_pos = get_search_pos(tags)
-	if search_pos is not None:
-		query = search_pos
+log = open('log.txt', 'a')
 
-	return query
-
-def handle_reply(b, request):
-	tries = 5
-
-	query = get_query(request['msg']['text'])
-
-	# Prevent duplicate replies
-	the_reply = b.reply(query)
-	while the_reply == request['msg']['text'] and tries > 0:
-		the_reply = b.reply(query)
-		tries = tries - 1
-
-	if the_reply == request['msg']['text']:
-		the_reply = 'what'
-	else:
-		# Replace any tailbot or birden references with the user's name
-		the_reply = re.sub(r'@?(?:tail\s?bot|(?:b|d)\Srd\Sn)', request['msg']['from']['first_name'], the_reply, flags=re.MULTILINE | re.IGNORECASE)
-
-	def delayed():
-		bot.sendChatAction(request['msg']['chat']['id'], 'typing')
-		time.sleep(.1 * len(the_reply))
-		if last_msg_id[request['msg']['chat']['id']] == request['msg']['message_id']:
-			request['sender'].sendMessage(the_reply, parse_mode='HTML')
-		else:
-			request['sender'].sendMessage(the_reply, parse_mode='HTML', reply_to_message_id=None if request['msg']['chat']['type'] == 'private' else request['msg']['message_id'])
-
-	if request['msg']['date'] + 10 < time.time():
-		print('I would\'ve responded, but the message was outside my reply threshold.')
-		return
-
-	thread = threading.Thread(target=delayed)
-	thread.daemon = True
-	thread.start()
-
-	spacing = ''
-	for i in range(len(request['msg']['from']['username']) + 6):
-		spacing = spacing + ' '
-	print('%sI replied: %r' % (spacing, the_reply.encode('UTF-8', errors='ignore')))
-
-def brain_function():
-	global reply_request
-
-	from cobe.brain import Brain
-	b = Brain("cobe.brain")
-
-	while True:
-		time.sleep(.1)
-		if len(reply_request) > 0:
-			for request in reply_request:
-				if request['msg']['text'].startswith('/'):
-					if request['msg']['text'] == '/info':
-						c = b.graph.cursor()
-
-						words = int(c.execute('SELECT COUNT(*) FROM tokens').fetchone()[0])
-						connections = int(c.execute('SELECT COUNT(*) FROM edges').fetchone()[0])
-						branches = int(c.execute('SELECT SUM(count) FROM nodes').fetchone()[0])
-						request['sender'].sendMessage('I know %s words and have a total of %s connections with %s possible branches between them.' % (words, connections, branches))
-					continue
-
-				for line in request['msg']['text'].split('\n'):
-					b.learn(line)
-
-				spacing = ''
-				for i in range(10 - len(request['msg']['chat']['type'])):
-					spacing = spacing + ' '
-				print('[%s]%s %r: %r' % (request['msg']['chat']['type'], spacing,
-								request['msg']['from']['username'].encode('UTF-8', errors='ignore'),
-								request['msg']['text'].encode('UTF-8', errors='ignore')))
-
-				last_msg_id[request['msg']['chat']['id']] = request['msg']['message_id']
-				if request['reply'] or (request['msg']['chat']['id'] in last_user_id and last_user_id[request['msg']['chat']['id']] == request['msg']['from']['id']):
-                                        if not request['reply'] and random() < .1:
-						del last_user_id[request['msg']['chat']['id']]
-					handle_reply(b, request)
-					last_user_id[request['msg']['chat']['id']] = request['msg']['from']['id']
-				elif request['msg']['chat']['id'] in last_user_id:
-					del last_user_id[request['msg']['chat']['id']]
-
-			reply_request = []
-
-thread = threading.Thread(target=brain_function)
-thread.daemon = True
-thread.start()
-
-class HALHandler(telepot.helper.ChatHandler):
+class ChatHandler(telepot.aio.helper.ChatHandler):
 	def __init__(self, *args, **kwargs):
-		super(HALHandler, self).__init__(*args, **kwargs)
+		super(ChatHandler, self).__init__(*args, **kwargs)
 
-	def on_chat_message(self, msg):
-		if 'text' not in msg or 'username' not in msg['from']:
+		self.conv_id = bot.storage.create_conversation()
+		
+		self.prev_msg = None
+
+	async def on_chat_message(self, msg):
+		botan.track(msg['from']['id'], msg)
+
+		if 'username' not in msg['from']:
 			return
+
+		data = get_data(msg)
+
+		if data is None: return
+
+		log.write(data.encode('UTF-8').decode('ascii', errors='ignore'))
 
 		reply = (msg['chat']['type'] == 'private')
 
-		if not reply and (random() < .03):
-			reply = True
-		if not reply and 'reply_to_message' in msg and 'username' in msg['reply_to_message']['from']:
-			if msg['reply_to_message']['from']['username'] == 'BlabberingFurryBot':
-				reply = True
-		new_text = re.sub('^(?:hey|hoi|sup|yo)?\s?(?:b|d)\Srd\Sn,?\s*?', '', msg['text'], flags=re.MULTILINE | re.IGNORECASE)
-		if msg['text'] != new_text:
-			msg['text'] = new_text
-			reply = True
+		if msg['date'] + 10 < time.time():
+			reply = False
 
-		reply_request.append({'sender': self.sender, 'msg': msg, 'reply': reply})
+		print('inp: %s' % data.encode('UTF-8').decode('ascii', errors='ignore'))
 
-bot = telepot.DelegatorBot('284668264:AAG_nlxgILhFZiB_IwwjwgCph6xn5bjwZnU', [
+		if 'reply_to_message' in msg:
+			self.prev_msg = get_data(msg['reply_to_message'])
+
+		if reply:
+			await tbot.sendChatAction(msg['chat']['id'], 'typing')
+			response = str(bot.get_response(data, self.conv_id))
+			print('bot: %s' % response.encode('UTF-8').decode('ascii', errors='ignore'))
+
+			if response.startswith('file.sticker='):
+				await self.sender.sendSticker(response.split('=', 2)[1])
+			elif response.startswith('file.document='):
+				await self.sender.sendDocument(response.split('=', 2)[1])
+			else:
+				await tbot.sendChatAction(msg['chat']['id'], 'typing')
+				await asyncio.sleep(2)
+				await self.sender.sendMessage(response)
+		elif self.prev_msg is not None:
+			bot.train([self.prev_msg, data])
+
+		self.prev_msg = data
+
+def get_data(msg):
+	data = None
+	if 'text' in msg: data = msg['text']
+	if 'sticker' in msg: data = 'file.sticker=' + msg['sticker']['file_id']
+	if 'document' in msg: data = 'file.document=' + msg['document']['file_id']
+	return data
+
+tbot = telepot.aio.DelegatorBot('284668264:AAG_nlxgILhFZiB_IwwjwgCph6xn5bjwZnU', [
 	pave_event_space()(
-		per_chat_id(), create_open, HALHandler, timeout=60
+		per_chat_id(), create_open, ChatHandler, timeout=20
 	)
 ])
-bot.message_loop(run_forever='Listening ...')
+
+loop = asyncio.get_event_loop() 
+loop.create_task(MessageLoop(tbot).run_forever())
+print('Listening...')
+loop.run_forever()
